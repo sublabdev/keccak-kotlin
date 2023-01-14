4765b08f766c953e5c620c41b4e393104acbdba1
TigranIskandaryan <tiger@sublab.dev>
2023-01-14 03:10:53 +0300

    Added documentation comments

--- src/main/kotlin/dev/sublab/keccak/Keccak.kt
+++ src/main/kotlin/dev/sublab/keccak/Keccak.kt
@@ -1,8 +1,15 @@
 package dev.sublab.keccak
 
+/**
+ * An interface for Keccak
+ */
 interface Keccak {
+    /**
+     * Returns `ByteArray` encrypted as Keccak f1600
+     */
     fun f1600(): ByteArray
 }
+
 
 private class KeccakImpl(
     private val byteArray: ByteArray
@@ -10,5 +17,8 @@ private class KeccakImpl(
     override fun f1600() = byteArray.normalize().toLongArray().f1600().toByteArray()
 }
 
+/**
+ * ByteArray to Keccak converter
+ */
 val ByteArray.keccak: Keccak
     get() = KeccakImpl(this)
