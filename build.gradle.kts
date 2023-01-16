e16172e465f28baeda968402a99ac8ac60c5e1c8
Alex Oakley <alex@alexoakley.com>
2023-01-15 18:42:27 -0800

    Replaced

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,12 +28,12 @@ tasks.test {
     useJUnitPlatform()
 }
 
-tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
-}
-
 tasks.dokkaHtml.configure {
     outputDirectory.set(projectDir.resolve("reference"))
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
 }
 
 val sourcesJar by tasks.registering(Jar::class) {
